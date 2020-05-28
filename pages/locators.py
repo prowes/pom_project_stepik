3e5767b631f76f333b270db3145e0b1c807f31a7
ivan prowes <locust94@gmail.com>
2020-05-28 20:00:23 +0300

    Added the login page tests

--- pages/locators.py
+++ pages/locators.py
@@ -1,11 +1,9 @@
 from selenium.webdriver.common.by import By
-
+from .base_page import BasePage
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-from .base_page import BasePage
-
-
 class LoginPageLocators():
-    
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTER_FORM = (By.ID, "register_form")
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,7 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
+from selenium import webdriver
+
 
 
 class LoginPage(BasePage):
@@ -7,14 +10,11 @@ class LoginPage(BasePage):
         self.should_be_login_form()
         self.should_be_register_form()
 
-    def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
-
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, f"No login mentioned in {self.browser.current_url}"
